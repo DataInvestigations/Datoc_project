73ec62dd1db1937c4e05d71c46f9efaef7ed0399
mayajian <mayajian@vt.edu>
2026-03-19 13:28:53 -0400

    Updated explanation

--- lm.R
+++ lm.R
@@ -36,13 +36,15 @@ check_model(fit, check = "qq")
 # which indicates that the assumption of linearity is likely satisfied. Given that 
 # the variance graph appears to display some curvature, the assumption of constant 
 # variance is violated. Additionally, the plot of residuals versus the fitted values 
-# shows that the points are not evenly distributed about the reference line. In terms of
-# the normality of the residuals, the majority of the points fall along the reference line 
-# and the qq plot looks relatively normal. However, there are some heavy tails occurring therefore 
-# it is more plausible that the assumption of normality is violated. Given the residuals versus 
-# leverage plot, there are three points that have potential to have significant influence on the 
-# regression line. However, these points do not exceed the cook's distance line. Also, on the 
-# influential observations plot, there are definitely some points displaying outliers. 
+# shows that the points are not evenly distributed about the reference line. This is 
+# likely occurring because the variance between the temperatures corresponding to each species 
+# is significant. In terms of the normality of the residuals, the majority of the points fall 
+# along the reference line and the qq plot looks relatively normal. However, there are some 
+# heavy tails occurring therefore it is more plausible that the assumption of normality is violated. 
+# Given the residuals versus leverage plot, there are three points that have potential to 
+# have significant influence on the regression line. However, these points do not exceed the 
+# cook's distance line. Also, on the influential observations plot, there are definitely 
+# some points indicative of outliers. 
 
 
 # Question 4
